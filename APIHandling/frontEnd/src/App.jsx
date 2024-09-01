31e582375043b82e3ca8e37dacd7c32e6c33c578
Sainath Shinde <shindesainath692@gmail.com>
2024-09-01 22:52:42 +0530

    Search added

--- APIHandling/frontEnd/src/App.jsx
+++ APIHandling/frontEnd/src/App.jsx
@@ -5,19 +5,47 @@ import axios from "axios";
 function App() {
   // if we need to use async awaiyt in useeffect for that we need to use ()() - immediately invoked functions - we cant use async await directly
 
-  const [products, error, loading] = customeReactQuery("/api/products");
+  // const [products, error, loading] = customeReactQuery("/api/products");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
 
-  if (loading) {
-    return <h1>Loading ....</h1>;
-  }
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get(`/api/products ? search` + search);
+        console.log("res", response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    })();
+  }, []);
 
-  if (error) {
-    return <h1>Something went wrong</h1>;
-  }
+  // if (loading) {
+  //   return <h1>Loading ....</h1>;
+  // }
+
+  // if (error) {
+  //   return <h1>Something went wrong</h1>;
+  // }
 
   return (
     <>
       <h1>New Js </h1>
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {loading && <h2>Loading ...</h2>}
+      {error && <h2>Something went wrong</h2>}
       <h2>Number of products are {products.length}</h2>
     </>
   );
