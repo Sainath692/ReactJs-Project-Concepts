e9e64d533cb0ed8eedd8e8c9afd8e6bd4251d9f6
Sainath Shinde <shindesainath692@gmail.com>
2024-10-03 18:06:13 +0530

    Form Validation Without YUP

---
+++ ReactYupForm/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormWithoutYup from "./component/form_without_yup";
+
+function App() {
+  return (
+    <div>
+      <FormWithoutYup />
+    </div>
+  );
+}
+
+export default App;
